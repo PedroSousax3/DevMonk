6fae25f948f26d6571e3cbe4d1ff6124c7ea6e4b
pedro sousa <pedro.sousax3@hotmail.com>
2021-07-11 20:09:57 -0300

    adicionando web

--- backend/services/conexao.js
+++ backend/services/conexao.js
@@ -11,6 +11,6 @@ async function conectarMongoDB () {
 module.exports = conectarMongoDB;
 
 
-mongosh "mongodb+srv://pedroadmin:TV76eap9Agwy9D3dnK@pedrosousa.online:27017" --username pedroadmin
-mongodb://pedroadmin:TV76eap9Agwy9D3dnK@pedrosousa.online:27017,pedrosousa.online:27017,pedrosousa.online:27017/main?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true
+//mongosh "mongodb+srv://pedroadmin:TV76eap9Agwy9D3dnK@pedrosousa.online:27017" --username pedroadmin
+//mongodb://pedroadmin:TV76eap9Agwy9D3dnK@pedrosousa.online:27017,pedrosousa.online:27017,pedrosousa.online:27017/main?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true
 
